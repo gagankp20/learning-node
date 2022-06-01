754d3901390e4401b62e3c94adc554951beaa956
gagankp20 <gagan749@gmail.com>
2022-06-02 00:20:10 +0530

    loading/require a module

---
+++ node-course/first-app/app.js
@@ -0,0 +1,6 @@
+const logger = require('./logger');
+logger.log('Message');
+
+//jshint node app.js -> tool used to show error. 
+
+
--- node-course/first-app/logger.js
+++ node-course/first-app/logger.js
@@ -4,3 +4,10 @@ function log(message){
 
 module.exports.log = log;
 //module.exports.someName = functionName
+
+/*
+    module.exports = log -> exports only the function. 
+        can be accessed directly with function name
+    module.exports.log = log -> exports an object
+        should be accessed with objectName.functionName
+*/

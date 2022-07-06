580e4140b4721cbae0c62f1bd56b86cbc26e4f9e
GAGAN K P <gagan749@gmail.com>
2022-07-06 23:49:46 +0530

    event arguements

--- node-course/first-app/app.js
+++ node-course/first-app/app.js
@@ -2,8 +2,8 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', ()=> {
-    console.log('Listener called')
+emitter.on('messageLogged', (arg)=> {
+    console.log('Listener called ',arg )
 })
 
-emitter.emit('messageLogged');
+emitter.emit('messageLogged',{id:1, name: 'Gagan'});

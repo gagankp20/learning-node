aa8ccb9304db24f410c8741279fdc0e0375bc439
GAGAN K P <gagan749@gmail.com>
2022-09-27 23:40:11 +0530

    async and await

--- node-course/async-demo/index.js
+++ node-course/async-demo/index.js
@@ -1,4 +1,4 @@
-// console.log('before');
+console.log('before');
 // getUser(1, (user) => {
 //     getRepositories(user.githubUserName, (repos) => {
 //      getCommits(repos[0], (commits) => {
@@ -7,12 +7,28 @@
 //     });
 // });
 
+//promise based approach
+// getUser(1)
+//     .then(user => getRepositories(user.githubUserName))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('commits', commits))
+//     .catch(err => console.log(err.message))
 
-getUser(1)
-    .then(user => getRepositories(user.githubUserName))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('commits', commits))
-    .catch(err => console.log(err.message))
+//async and await approach
+async function displayCommits() {
+    try{
+        const user = await getUser(1)
+        const repos = await getRepositories(user.githubUserName)
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch(err){
+        console.log('error', err.message);
+    }
+    
+}
+
+displayCommits();
 
 console.log('after')
 
@@ -34,6 +50,7 @@ console.log('after')
         setTimeout(() => {
             console.log('repo list');
             resolve(['repo1', 'repo2', 'repo3']);
+           //reject(new Error('couldnnot'))
         },2000);
     });
 }

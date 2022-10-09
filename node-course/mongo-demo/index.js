323d85776f8f54e2035b63a8a9144b0a619cf90d
GAGAN K P <gagan749@gmail.com>
2022-10-09 17:43:56 +0530

    Querying and operators

--- node-course/mongo-demo/index.js
+++ node-course/mongo-demo/index.js
@@ -27,4 +27,20 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+    const pageNumber =2;
+    const pageSize = 10;
+
+
+    const course = await Course
+    .find({author:'Mosh', isPublished: true})                           //general         .count() returns count
+    //.find({author: /^Mosh/i})                                         regular expression. i is case insensitive 
+    // .or([{author: 'Mosh'}, {isPublished: true}])                     logical operation
+    .skip((pageNumber-1)* pageSize)
+    .limit(pageSize)
+    .sort({name: 1})
+    .select({name:1, tags:1});
+    console.log(course);
+}
+
+getCourses();
